da5aca67aafb5548757a6dc4d6ef37ef9bcb6126
Saúl Camacho <electrohead1200@gmail.com>
2023-11-25 19:49:51 -0600

    Configurando Pug y Layout de las Vistas

--- app.js
+++ app.js
@@ -1,18 +1,22 @@
 import express from "express";
 import path from "path";
+import { fileURLToPath } from "url"
 import cors from "cors";
 import helmet from "helmet";
 import morgan from "morgan";
 import taskController from "./controllers/taskController.js";
 import errorController from "./controllers/errorController.js";
 
-const __dirname = path.dirname(new URL(import.meta.url).pathname);
+const __dirname = fileURLToPath(new URL(".", import.meta.url));
 const app = express();
 const port = 3000;
 
 app.use(cors());
 app.use(helmet());
 app.use(morgan("dev"));
+
+app.set("views", path.join(__dirname, "views")); // Indica carpeta de las Vistas
+app.set("view engine", "pug");// Indica el template Engine
 
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
